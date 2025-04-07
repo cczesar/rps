4dd4873abe39e4e381e6a55b9bef45935816002d
cczesar <201019467+cczesar@users.noreply.github.com>
2025-04-07 16:15:22 -0600

    Added the function to the game and added tolowercase

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-console.log("Rock Paper Scissors Game");
+console.log('Rock Paper Scissors Game');
 
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    const choice = options[(Math.random() * options.lenght)];
+    const choice = options[Math.floor(Math.random() * options.length)];
+    console.log(`Computer choice: ${choice}`);
+    return choice;
 }
 
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+        return "Player win";
+    } else {
+        return "Computer Win";
+    }
+}
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
+    if (result === "Tie") {
+        return "It's a tie!";
+    } else if (result === "Player win") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = prompt("rock, paper, or scissors:").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
